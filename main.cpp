5bcb7cbb7521d641838ffe5ac6d1d9b6afa1e3e7
Alex Rolfe <github@valistar.pro>
2015-09-30 16:51:12 -0700

    Remove access credentials

--- main.cpp
+++ main.cpp
@@ -4,17 +4,35 @@
 
 #include "src/aws/request.h"
 #include "src/aws/get_request.h"
+#include "src/aws/post_request.h"
 
 
 using namespace std;
 
 int main() {
-    curl_global_init(CURL_GLOBAL_ALL);
-    aws::GetRequest request_test;
-    request_test.setUrl("http://mydevices/awstest?query1=one&query2=two");
-    request_test.setHeader("NotRealHeader", "NotRealValue");
-    request_test.send();
+  curl_global_init(CURL_GLOBAL_ALL);
+  /*
+  aws::GetRequest request_test;
+  request_test.setUrl("https://glacier.us-east-1.amazonaws.com/-/vaults");
+  request_test.selectRegion("us-east-1");
+  request_test.selectService("glacier");
+  request_test.setAccessKey("access");
+  request_test.setSecretKey("sekret");
+  request_test.setHeader("x-amz-glacier-version", "2012-06-01");
+  request_test.send();
+   */
 
-    cout << endl << "Goodbye, World!" << endl;
-    return 0;
+  aws::PostRequest request_test;
+  //request_test.setUrl("https://glacier.us-east-1.amazonaws.com/-/vaults/test_vault_manual/archives");
+  request_test.setUrl("http://requestb.in/168jz8g1");
+  request_test.selectRegion("us-east-1");
+  request_test.selectService("glacier");
+  request_test.setAccessKey("access"); //No, the old keys here arent valid.
+  request_test.setSecretKey("sekret");
+  request_test.setHeader("x-amz-glacier-version", "2012-06-01");
+  request_test.addBodyFile("F:\\CPP\\Glacier\\tfile.txt");
+  request_test.send();
+
+  cout << endl << "Goodbye, World!" << endl;
+  return 0;
 }
